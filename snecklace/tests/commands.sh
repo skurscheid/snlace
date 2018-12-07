6b3ec081acdfee0460e3dd84cdbe614df8538ea9
sarahhp <sarah@JCSMR-043221WD.UDS.anu.edu.au>
2018-12-07 15:01:17 +1100

    Including pblat module

---
+++ snecklace/tests/commands.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#cd /home/output_data/workflows
+
+#pre-trim dryrun
+#snakemake -s trim_subsample.py -d /home --configfile config.json --cores 1 -npr 
+#necklace dryrun
+snakemake -s snecklace/Snakefile -d ../ --configfile snecklace/tests/dryruntest.json --cores 1 --use-conda -npr
+
+#trim datasets: testes, brain and round spermatids. 
+#In reality the brain and spermatid filenames were changed from the raw files as a 
+#part of trimming and preprocessing
+#snakemake -s trim_subsample.py -d /home --configfile config.json --cores 40 -pr
+
+#necklace test case
+#snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' --cores 40 --use-conda -pr
+
+#conda test
+#conda create -f snecklace/envs/lace.yml --name lace
+
+#symlink test
+#ln -s config.json master.config.json 
